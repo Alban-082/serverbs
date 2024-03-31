0dbb25ae5052553bea21a5a809cadbf5ec1840bf
Alban-082 <165586885+Alban-082@users.noreply.github.com>
2024-03-31 20:44:09 +0200

    Add files via upload

---
+++ Protocol/LogicCommandManager.py
@@ -0,0 +1,24 @@
+from Protocol.Commands.Client.LogicSelectSkinCommand import LogicSelectSkinCommand
+from Protocol.Commands.Client.LogicSetPlayerThumbnailCommand import LogicSetPlayerThumbnailCommand
+from Protocol.Commands.Client.LogicSetPlayerNameColorCommand import LogicSetPlayerNameColorCommand
+from Protocol.Commands.Client.LogicPurchaseDoubleCoinsCommand import LogicPurchaseDoubleCoinsCommand
+from Protocol.Commands.Client.LogicPurchaseHeroLvlUpMaterialCommand import LogicPurchaseHeroLvlUpMaterialCommand
+from Protocol.Commands.Client.LogicPurchaseBrawlPassCommand import LogicPurchaseBrawlPassCommand
+from Protocol.Commands.Client.LogicPurchaseOfferCommand import LogicPurchaseOfferCommand
+from Protocol.Commands.Client.LogicGatchaCommand import LogicGatchaCommand
+from Protocol.Commands.Client.LogicLevelUpCommand import LogicLevelUpCommand
+from Protocol.Commands.Client.LogicSelectEmoteCommand import LogicSelectEmoteCommand
+
+commands = {
+	500: LogicGatchaCommand,
+	505: LogicSetPlayerThumbnailCommand,
+	506: LogicSelectSkinCommand,
+	509: LogicPurchaseDoubleCoinsCommand,
+	519: LogicPurchaseOfferCommand,
+	520: LogicLevelUpCommand,
+	521: LogicPurchaseHeroLvlUpMaterialCommand,
+	527: LogicSetPlayerNameColorCommand,
+	534: LogicPurchaseBrawlPassCommand,
+    538: LogicSelectEmoteCommand
+
+}
